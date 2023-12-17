bb2ff999fd30d03a0f61d0587d7bd905dcd97a6a
Jens Sager <jens.sager@uni-oldenburg.de>
2023-12-17 14:16:13 +0100

    finish topology function

--- scenario_setup/make_topology.py
+++ scenario_setup/make_topology.py
@@ -1,5 +1,6 @@
 # given n agents, small world k, and an rng seed, output a
 # small world topology of n agents
+import random
 
 
 def make_small_world_topology(n_agents, k, p, seed):
@@ -14,10 +15,35 @@ def make_small_world_topology(n_agents, k, p, seed):
     # @pyinclude(my_dir * "/scenario_setup/make_topology.py")
     # py"make_small_world_topology(1,2,3,4)"
 
+    # create a ring of size n_agents
+    # and connect every k distance neighbors
+    adjacency_matrix = [[0 for _ in range(n_agents)] for _ in range(n_agents)]
+
+    for i in range(n_agents):
+        for j in range(n_agents):
+            if i == j:
+                # agents are not their own neighbor
+                continue
+
+            if abs((i - j) % n_agents) <= k or abs(i - j) <= k:
+                adjacency_matrix[i][j] = 1
+
+    # add more edges with a random chance
+    random.seed(seed)
+
+    for i in range(n_agents):
+        for j in range(n_agents):
+            if i == j:
+                # agents are not their own neighbor
+                continue
+
+            if adjacency_matrix[i][j] == 0:
+                if random.random() < p:
+                    adjacency_matrix[i][j] = 1
+
     # output: n x n adjacency matrix
-    return [
-        [1, 2, 3, 4, 5, 6, 7, 8],
-        [1, 2, 3, 4, 5, 6, 7, 8],
-        [1, 2, 3, 4, 5, 6, 7, 8],
-        [1, 2, 3, 4, 5, 6, 7, 8],
-    ]
+    return adjacency_matrix
+
+
+if __name__ == "__main__":
+    print(make_small_world_topology(10, 2, 0.5, 123))
